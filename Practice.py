1223a0a2a16de99bdd413a416fe3d0b7cbf29e30
Rares <rares1928@gmail.com>
2020-07-27 01:42:15 +0300

    varianta care are picioare

--- Practice.py
+++ Practice.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import json
 from selenium.webdriver import ActionChains
 
 from selenium.common.exceptions import StaleElementReferenceException, ElementClickInterceptedException
@@ -7,25 +8,30 @@ from selenium.common.exceptions import StaleElementReferenceException, ElementCl
 options=webdriver.ChromeOptions()
 options.add_argument("--headless")
 
-browser=webdriver.Chrome(options=options)
+browser=webdriver.Chrome()
 browser.get('https://www.flashscore.ro/fotbal/anglia/premier-league-2018-2019/rezultate/')
 buton=browser.find_element_by_class_name('event__more')
-actions = ActionChains(browser)
-actions.move_to_element(buton).perform()
+# actions = ActionChains(browser)
+# actions.move_to_element(buton).perform()
+browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+j=1
 while True:
     try:
         browser.implicitly_wait(10)
         buton.click()
         browser.implicitly_wait(10)
-        actions = ActionChains(browser)
-        actions.move_to_element(buton).perform()
-        time.sleep(3)
+        # actions = ActionChains(browser)
+        # actions.move_to_element(buton).perform()
+        browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        print('am facut '+str(j)+' click-uri')
+        j+=1
+        time.sleep(3.5)
     except :
         break
 
 meciuri_de_salvat=[]
 meciuri=browser.find_elements_by_class_name("event__match")
-if(len(meciuri_de_salvat)!=380):
+if(len(meciuri)!=380):
     print('am failat la viata')
     print('am luat '+str(len(meciuri))+' meciuri')
     browser.quit()
@@ -60,6 +66,8 @@ for meci in meciuri:
     meciuri_de_salvat[i]['fixture']=div_date.text.split()[5]
     meciuri_de_salvat[i]['date_time']=div_date.text.split()[6], div_date.text.split()[7]
 
+    tab_nou.implicitly_wait(10)
+    print(tab_nou.current_url)
     div_tabel=tab_nou.find_element_by_id('odds_1x2')
     l_odd=div_tabel.find_elements_by_class_name('odd')
     l_even=div_tabel.find_elements_by_class_name('even')
@@ -67,11 +75,11 @@ for meci in meciuri:
     tab_nou.implicitly_wait(10)
     for casa in lista_case:
         meciuri_de_salvat[i][casa.find_element_by_class_name('elink').get_attribute('title')]=casa.text.split()
-    i+=1
     tab_nou.quit()
     # browser.switch_to_window(browser.window_handles[0])
-    file.write(meciuri_de_salvat[i])
+    file.write(json.dumps(meciuri_de_salvat[i]))
     print('am facut '+str(i)+' meciuri')
     time.sleep(1)
+    i += 1
 
 file.close()
