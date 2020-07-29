35f05246dcd636c7258fb557160ecee802c37259
mateimanea <56696113+mateimanea@users.noreply.github.com>
2020-07-29 16:10:53 +0300

    Excel Done

--- Excel_Writer.py
+++ Excel_Writer.py
@@ -470,11 +470,103 @@ def excel_2018_2019(file):
     shutil.move(os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//text files", file[:-3] + 'xlsx'),
                 os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//excel files", file[:-3] + 'xlsx'))
 
+
+def excel_for_all(file):
+    f = open(file)
+    data = json.load(f)
+    # toate casele de pariuri
+    case_pariuri = list(data[0].keys())[5:]
+    print(case_pariuri)
+    round, date, home, result, away = ([] for i in range(5))
+    # unibet, betfair, winmasters, betano, fortuna, netbet, bet365 = ([] for i in range(12))
+    cote_dict = {casa: [] for casa in case_pariuri}
+    print(cote_dict)
+    headers = ["Date", "Round", "Home", "Result", "Away"] + ["1", "X", "2"] * len(case_pariuri)
+    # case = ["Unibet", "Betfair", "NetBet", "Betano", "Fortuna", "Winmasters"]
+
+    for meci in data:
+        # print(meci["Winmasters.ro"])
+        # print(meci["Fortuna.ro"])
+        try:
+            round.append(meci["fixture"])
+        except KeyError:
+            round.append(" ")
+        try:
+            date.append(meci["date_time"][0])
+        except KeyError:
+            date.append(" ")
+        try:
+            home.append(meci["home_team"])
+        except KeyError:
+            home.append(" ")
+        try:
+            result.append(meci["result"])
+        except KeyError:
+            result.append(" ")
+        try:
+            away.append(meci["away_team"])
+        except KeyError:
+            away.append(" ")
+        for i in range(len(case_pariuri)):
+            try:
+                cote_dict[case_pariuri[i]].append(meci[case_pariuri[i]])
+            except KeyError:
+                cote_dict[case_pariuri[i]].append([" ", " ", " "])
+
+    print(cote_dict)
+    print(len(cote_dict[case_pariuri[0]]))
+    # [print(len(cote_dict[case_pariuri[i]])) for i in range(len(case_pariuri))]
+
+    workbook = xlsxwriter.Workbook(file[:-3] + 'xlsx')
+    worksheet = workbook.add_worksheet()
+    nume_excel = "Premier League " + file[-9:-4]
+    print(nume_excel)
+    bold = workbook.add_format({'bold': True})
+    middle = workbook.add_format()
+    middle.set_align('center')
+    bold.set_align('center')
+    worksheet.set_column('A:A', max_length(date), middle)
+    worksheet.set_column('C:C', max_length(home))
+    worksheet.set_column('E:E', max_length(away))
+    worksheet.set_column(5, len(case_pariuri), max_length(cote_dict[case_pariuri[0]]) + 3)
+
+    row = 1
+    column = 0
+
+    worksheet.merge_range(0, 0, 0, 4, nume_excel, bold)
+    count = 0
+    for casa in case_pariuri:
+        worksheet.merge_range(0, 5 + count, 0, 7 + count, casa, bold)
+        count += 3
+
+    for header in headers:
+        worksheet.write(row, column, header, bold)
+        column += 1
+
+    row = 2
+    for i in range(len(date)):
+        worksheet.write(row + i, 0, date[i])
+        worksheet.write(row + i, 1, round[i], middle)
+        worksheet.write(row + i, 2, home[i])
+        worksheet.write(row + i, 3, "-".join(result[i]), middle)
+        worksheet.write(row + i, 4, away[i])
+        k_step = 5
+        for k in range(len(case_pariuri)):
+            for j in range(3):
+                worksheet.write(row + i, k_step + j, cote_dict[case_pariuri[k]][i][j], middle)
+            k_step += 3
+
+    workbook.close()
+    shutil.move(os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//text files", file[:-3] + 'xlsx'),
+                os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//excel files", file[:-3] + 'xlsx'))
+    # shutil.move(os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//text files", file[:-4] + '_test.xlsx'),
+    #             os.path.join("C://Users//Matei//Documents//GitHub//Flashscore//excel files", file[:-4] + '_test.xlsx'))
 os.chdir(os.path.join("C://Users//Matei//Documents//GitHub//Flashscore", "text files"))
 print(os.getcwd())
 #file = "meciuri_salvate_19-20.txt"
 #file = "meciuri_salvate_corect_18-19.txt"
-#file = "meciuri_salvate_17-18.txt"
+file = "meciuri_salvate_17-18.txt"
 #file = "meciuri_salvate_16-17.txt"
-file = "meciuri_salvate_corect_18-19.txt"
-excel_2018_2019(file)
+#file = "meciuri_salvate_corect_18-19.txt"
+#excel_2018_2019(file)
+excel_for_all(file)
