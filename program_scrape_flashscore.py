ff2f6cd4033b5c4c9a38dc63c4f3a3d3c3b9cfbd
rares1928 <46875836+rares1928@users.noreply.github.com>
2020-07-29 03:11:07 +0300

    update FUCKING CORECT

--- program_scrape_flashscore.py
+++ program_scrape_flashscore.py
@@ -23,6 +23,7 @@ while True:
         break
 
 meciuri_de_salvat=[]
+browser.implicitly_wait(10)
 meciuri=browser.find_elements_by_class_name("event__match")
 if(len(meciuri)!=380):
     print('am failat la viata')
@@ -40,29 +41,61 @@ for meci in meciuri:
 
     try:
         tab_nou=webdriver.Chrome(options=options)
+        tab_nou.implicitly_wait(10)
         link='https://www.flashscore.ro/meci/'+meci.get_attribute('id')[4:]+'/#comparare-cote;cote-1x2;final'
         tab_nou.get(link)
 
         tab_nou.implicitly_wait(10)
         div_home=tab_nou.find_element_by_class_name('home-box')
+        tab_nou.implicitly_wait(10)
         div_away=tab_nou.find_element_by_class_name('away-box')
+        tab_nou.implicitly_wait(10)
         div_scor=tab_nou.find_element_by_class_name('current-result')
+        tab_nou.implicitly_wait(10)
         div_date=tab_nou.find_element_by_class_name('description')
+        tab_nou.implicitly_wait(10)
 
         meciuri_de_salvat[i]['home_team']=div_home.text
-        meciuri_de_salvat[i]['away_team']=div_away.text
-        meciuri_de_salvat[i]['result']=div_scor.text.split('\n-')
-        meciuri_de_salvat[i]['fixture']=div_date.text.split()[5]
-        meciuri_de_salvat[i]['date_time']=div_date.text.split()[6], div_date.text.split()[7]
+        while len(meciuri_de_salvat[i]['home_team'])==0:
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['home_team']=div_home.text
+            print('am facut while la home team')
+        print(meciuri_de_salvat[i]['home_team'])
+
 
         tab_nou.implicitly_wait(10)
-        print(tab_nou.current_url)
+        meciuri_de_salvat[i]['away_team']=div_away.text
+        while len(meciuri_de_salvat[i]['away_team'])==0:
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['away_team']=div_away.text
+            print('am facut while la away team')
+        print(meciuri_de_salvat[i]['away_team'])
+        tab_nou.implicitly_wait(10)
+        meciuri_de_salvat[i]['result']=div_scor.text.split('\n-')
+        while len(meciuri_de_salvat[i]['result'])!=2:
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['result']=div_scor.text.split('\n-')
+            print('am intrat in while la result')
+        print(meciuri_de_salvat[i]['result'])
+        tab_nou.implicitly_wait(10)
+        meciuri_de_salvat[i]['fixture']=div_date.text.split()[5]
+        print(meciuri_de_salvat[i]['fixture'])
+        tab_nou.implicitly_wait(10)
+        meciuri_de_salvat[i]['date_time']=div_date.text.split()[6], div_date.text.split()[7]
+        print(meciuri_de_salvat[i]['date_time'])
+        tab_nou.implicitly_wait(10)
+
+        tab_nou.implicitly_wait(10)
+        tab_nou.implicitly_wait(10)
         div_tabel=tab_nou.find_element_by_id('odds_1x2')
+        tab_nou.implicitly_wait(10)
         l_odd=div_tabel.find_elements_by_class_name('odd')
+        tab_nou.implicitly_wait(10)
         l_even=div_tabel.find_elements_by_class_name('even')
         lista_case=l_odd+l_even
         tab_nou.implicitly_wait(10)
         for casa in lista_case:
+            tab_nou.implicitly_wait(10)
             meciuri_de_salvat[i][casa.find_element_by_class_name('elink').get_attribute('title')]=casa.text.split()
         tab_nou.quit()
         print('am facut '+str(i)+' meciuri')
