78db72a1c186fb8fb508a5de7728f2f0dba84edd
rares1928 <46875836+rares1928@users.noreply.github.com>
2020-07-29 03:14:03 +0300

    update corect

--- program_scrape_flashscore.py
+++ program_scrape_flashscore.py
@@ -6,7 +6,7 @@ options=webdriver.ChromeOptions()
 options.add_argument("--headless")
 
 browser=webdriver.Chrome()
-browser.get('https://www.flashscore.ro/fotbal/anglia/premier-league-2019-2020/rezultate/')
+browser.get('https://www.flashscore.ro/fotbal/anglia/premier-league-2018-2019/rezultate/')
 buton=browser.find_element_by_class_name('event__more')
 browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 j=1
@@ -105,6 +105,6 @@ for meci in meciuri:
         print('am ratat meciul: '+str(len(meciuri_de_salvat)))
         continue
 
-file=open('meciuri_salvate_19-20.txt','w')
+file=open('meciuri_salvate_18-19(2).txt','w')
 file.write(json.dumps(meciuri_de_salvat))
 file.close()
