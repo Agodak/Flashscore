4b685397b38a8d502da7f7a999832980196f5d61
rares1928 <46875836+rares1928@users.noreply.github.com>
2020-08-04 00:04:09 +0300

    Create programel_scrape_bundes.py

---
+++ programel_scrape_bundes.py
@@ -0,0 +1,120 @@
+from selenium import webdriver
+import time
+import json
+
+def bully_scrap(sezon,salvare):
+    options=webdriver.ChromeOptions()
+    options.add_argument("--headless")
+
+    browser=webdriver.Chrome()
+    browser.get(sezon)
+    buton=browser.find_element_by_class_name('event__more')
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    j=1
+    while True:
+        try:
+            browser.implicitly_wait(10)
+            buton.click()
+            browser.implicitly_wait(10)
+            browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            print('am facut '+str(j)+' click-uri')
+            j+=1
+            time.sleep(5)
+        except :
+            break
+
+    meciuri_de_salvat=[]
+    browser.implicitly_wait(10)
+    meciuri=browser.find_elements_by_class_name("event__match")
+    if(len(meciuri)!=308 and len(meciuri)!=306):
+        print('am failat la viata')
+        print('am luat '+str(len(meciuri))+' meciuri')
+        browser.quit()
+        exit()
+    else:
+        print('am luat tot manca-ti-as')
+
+    i=0
+    if sezon!='https://www.flashscore.ro/fotbal/germania/bundesliga-2011-2012/rezultate/':
+        meciuri=meciuri[2:]
+    for meci in meciuri:
+        meciuri_de_salvat.append({})
+        browser.implicitly_wait(10)
+
+        try:
+            tab_nou=webdriver.Chrome(options=options)
+            tab_nou.implicitly_wait(10)
+            link='https://www.flashscore.ro/meci/'+meci.get_attribute('id')[4:]+'/#comparare-cote;cote-1x2;final'
+            tab_nou.get(link)
+
+            tab_nou.implicitly_wait(10)
+            div_home=tab_nou.find_element_by_class_name('home-box')
+            tab_nou.implicitly_wait(10)
+            div_away=tab_nou.find_element_by_class_name('away-box')
+            tab_nou.implicitly_wait(10)
+            div_scor=tab_nou.find_element_by_class_name('current-result')
+            tab_nou.implicitly_wait(10)
+            div_date=tab_nou.find_element_by_class_name('description')
+            tab_nou.implicitly_wait(10)
+
+            meciuri_de_salvat[i]['home_team']=div_home.text
+            while len(meciuri_de_salvat[i]['home_team'])==0:
+                tab_nou.implicitly_wait(10)
+                meciuri_de_salvat[i]['home_team']=div_home.text
+                print('am facut while la home team')
+            print(meciuri_de_salvat[i]['home_team'])
+
+
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['away_team']=div_away.text
+            while len(meciuri_de_salvat[i]['away_team'])==0:
+                tab_nou.implicitly_wait(10)
+                meciuri_de_salvat[i]['away_team']=div_away.text
+                print('am facut while la away team')
+            print(meciuri_de_salvat[i]['away_team'])
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['result']=div_scor.text.split('\n-')
+            while len(meciuri_de_salvat[i]['result'])!=2:
+                tab_nou.implicitly_wait(10)
+                meciuri_de_salvat[i]['result']=div_scor.text.split('\n-')
+                print('am intrat in while la result')
+            print(meciuri_de_salvat[i]['result'])
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['fixture']=div_date.text.split()[4]
+            print(meciuri_de_salvat[i]['fixture'])
+            tab_nou.implicitly_wait(10)
+            meciuri_de_salvat[i]['date_time']=div_date.text.split()[5], div_date.text.split()[6]
+            print(meciuri_de_salvat[i]['date_time'])
+            tab_nou.implicitly_wait(10)
+
+            tab_nou.implicitly_wait(10)
+            tab_nou.implicitly_wait(10)
+            div_tabel=tab_nou.find_element_by_id('odds_1x2')
+            tab_nou.implicitly_wait(10)
+            l_odd=div_tabel.find_elements_by_class_name('odd')
+            tab_nou.implicitly_wait(10)
+            l_even=div_tabel.find_elements_by_class_name('even')
+            lista_case=l_odd+l_even
+            tab_nou.implicitly_wait(10)
+            for casa in lista_case:
+                tab_nou.implicitly_wait(10)
+                meciuri_de_salvat[i][casa.find_element_by_class_name('elink').get_attribute('title')]=casa.text.split()
+            tab_nou.quit()
+            print('am facut '+str(i)+' meciuri')
+            time.sleep(1)
+            i += 1
+        except:
+            print('am ratat meciul: '+str(len(meciuri_de_salvat))+ ' din sezonul:')
+            print(sezon)
+            continue
+
+    file=open(salvare,'w')
+    file.write(json.dumps(meciuri_de_salvat))
+    file.close()
+    browser.quit()
+
+lista_link = ['https://www.flashscore.com/football/germany/bundesliga-20{}-20{}/results/'.format(i, i + 1) for i in range(10, 20)]
+lista_salvari=['meciuri_bundesliga_{}-{}.txt'.format(i,i+1) for i in range(10,20)]
+
+for j in range(10):
+    bully_scrap(lista_link[j],lista_salvari[j])
